1a4cd364ee9a8e3ffbea137727702b7e025671ad
alkozko <alkozko@yandex.ru>
2014-08-14 09:20:55 +0600

    Implement algorithm

---
+++ src/Program.scala
@@ -0,0 +1,31 @@
+import com.sun.jmx.snmp.Timestamp
+
+/**
+ * Created by alkozko on 13.08.2014.
+ */
+object Program {
+  def main(args: Array[String]): Unit = {
+    val field = new LifeField(10,10,15);
+    while (true)
+    {
+      printField(field);
+//      io.StdIn.readLine();
+      field.toNextState;
+      Thread.sleep(500);
+    }
+  }
+
+  def printField(field: LifeField) {
+    for (i <- 0 until 10) {
+      for (j <- 0 until 10) {
+        if (field.state(i)(j))
+          print("■ ")
+        else
+          print("□ ");
+      }
+      println();
+    }
+    println("==" * 20)
+  }
+}
+
